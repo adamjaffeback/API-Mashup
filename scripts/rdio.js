19e52b44adfba9fa2e7bfb43d62772c310aebcad
Greg Ferrell <gregerg@gmail.com>
2014-10-02 21:15:26 -0700

    added necessary helper.html file, and started working on playlist functionality

--- scripts/rdio.js
+++ scripts/rdio.js
@@ -7,11 +7,37 @@ R.ready(function() {
     },
     success: function(result) {
       var tracks = result.result.results;
+
+      // play one song:
       var numTracks = tracks.length;
       var rand = Math.floor(Math.random()*numTracks);
       var track = tracks[rand];
       var key = track.key;
       R.player.play({source: key});
+
+      // play several songs. Can't do from localhost?
+      // savePlaylist(tracks);
     }
   });
 });
+
+
+var savePlaylist = function(tracks) {
+  var self = this;
+  R.request({
+    method: 'createPlaylist',
+    content: {
+      name: "SpaceMash",
+      description: 'Created with API call',
+      tracks: tracks
+    },
+    success: function(result) {
+      var playPlaylistKey = result.result.key;
+      playPlaylist(playlistKey);
+    }
+  });
+};
+
+var playPlaylist = function(playListKey) {
+  R.player.play({ source: playlistKey });
+};
