0401b0bb0684ee9d805a9245121f6a0b598cb76e
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-10-02 21:05:28 -0700

    created styles folder

--- scripts/maps.js
+++ scripts/maps.js
@@ -5,19 +5,20 @@ var longitude = 0;
 var map;
 
 function initialize() {
-  var mapOptions = {
-    zoom: 2,
-    center: new google.maps.LatLng(20, 0)
-  };
+  	var mapOptions = {
+    	zoom: 2,
+    	center: new google.maps.LatLng(20, 0)
+  	};
   
-  map = new google.maps.Map(document.getElementById('map-canvas'),
+  	map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
 
-//create marker
-var marker = new google.maps.Marker({
+	//create marker
+	var marker = new google.maps.Marker({
         position: new google.maps.LatLng(latitude, longitude),
         map: map,
-        title: 'ISS'
-});
-      }
+        title: 'ISS',
+        icon: image,
+	});
+}
